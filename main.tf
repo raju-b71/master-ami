466a0e985e78f060cbd41f41724a774e1518b83a
rajendra <rajendrammarboyana@gmail.com>
2024-05-15 13:55:11 +0100

    creating ami ec2

--- main.tf
+++ main.tf
@@ -23,3 +23,11 @@ resource "null_resource" "ansible" {
   }
 
 }
+
+resource "aws_ami_from_instance" "ami" {
+  depends_on         = [null_resource.ansible]
+  name               = "golden-ami-${formatdate("DD-MM-YY", timestamp())}"
+  source_instance_id = aws_instance.ami.id
+
+
+}
